693a835dfcfa584b45279cd2a5f538dc8c15cd37
Matthew McIvor <matthewrmcivor@gmail.com>
2024-09-11 22:34:19 -0700

    got .env and api key working

--- baseline-app-backend/server.js
+++ baseline-app-backend/server.js
@@ -1,16 +1,23 @@
+const path = require('path');
 const express = require('express');
 const axios = require('axios');
 const app = express();
 const dotenv = require('dotenv');
-dotenv.config();
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 
 const PORT = process.env.PORT || 3000;
 
 
-// const SPOONACULAR_API_KEY = process.env.SPOONACULAR_API_KEY;
+const SPOONACULAR_API_KEY = process.env.SPOONACULAR_API_KEY;
 
 app.use(express.json());
+
+// console.log("SPOONACULAR_API_KEY=",SPOONACULAR_API_KEY)
+
+
+
 
 // Start the server
 app.listen(PORT, () => {
